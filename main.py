4d225b7914abf926c035590af749f254853e996f
k4my-git <kambara0229@icloud.com>
2021-10-07 14:36:02 +0900

    update

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ def callback():
  
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
