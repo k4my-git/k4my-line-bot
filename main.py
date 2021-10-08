6e056299fa82e2346d4df42633d371eab6c8e2b4
k4my-git <kambara0229@icloud.com>
2021-10-08 16:29:17 +0900

    update

--- main.py
+++ main.py
@@ -49,9 +49,10 @@ def callback():
  
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
+    if event.message.text == "test":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event))
  
 # ポート番号の設定
 if __name__ == "__main__":
