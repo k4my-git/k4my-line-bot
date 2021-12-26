44eaf539aedaf45917c5a2dd53140a5420266a32
k4my-git <kambara0229@icloud.com>
2021-12-26 16:56:38 +0900

    update

--- hololive.py
+++ hololive.py
@@ -1,167 +1,169 @@
-from bs4 import BeautifulSoup as bs
-import requests
-import lxml,html5lib
-from youtubesearchpython import *
 import time
 
+import requests
+from bs4 import BeautifulSoup as bs
+from youtubesearchpython import *
+
+
 def scrape():
+    global urls, times, img, icons
     link = 'https://schedule.hololive.tv/'
 
     res = requests.get(link)
     try:
         soup = bs(res.text, "lxml")
     except:
-    	soup = bs(res.text, "html5lib")
-    
-    data=[]
+        soup = bs(res.text, "html5lib")
 
-    for ele in soup.select('.col-6.col-sm-4.col-md-3'):#('a[style*="solid"]'):
-    	if 'border: 3px red' in str(ele):
-    		for y_url in ele.select('a[class=thumbnail]'):
-    			urls = y_url['href']
-    		for y_time in ele.select('div[class*=col-5]'):
-    			times = y_time.get_text().replace(" ","").replace("\n","")+"～"
-    			#print("start >"+times)
-    		for y_name in ele.select('div[class*=name]'):
-    			names = y_name.get_text().replace(" ","").replace("\n","")
-    			#print("name >"+names)
-    		for thumbnail in ele.select('div[class*=col-12] > img'):
-    			img = thumbnail['src']
-    			#print("thumbnail >"+img)
-    		count = 0
-    		for y_icon in ele.select('div[class*=col-xl] > img'):
-    			if count == 0:
-    				icons = y_icon['src']
-    				count+=1
+    data = []
 
-    		time.sleep(1)
-    		print(urls)
-    		y_data = youtubes(urls)
+    for ele in soup.select('.col-6.col-sm-4.col-md-3'):  # ('a[style*="solid"]'):
+        if 'border: 3px red' in str(ele):
+            for y_url in ele.select('a[class=thumbnail]'):
+                urls = y_url['href']
+            for y_time in ele.select('div[class*=col-5]'):
+                times = y_time.get_text().replace(" ", "").replace("\n", "") + "～"
+            for y_name in ele.select('div[class*=name]'):
+                names = y_name.get_text().replace(" ", "").replace("\n", "")
+            for thumbnail in ele.select('div[class*=col-12] > img'):
+                img = thumbnail['src']
+            count = 0
+            for y_icon in ele.select('div[class*=col-xl] > img'):
+                if count == 0:
+                    icons = y_icon['src']
+                    count += 1
 
-    		data.append(dict(url=urls,name=y_data['name'],time=times,image=img,icon=icons,title=y_data['title'],count=y_data['viewcount'],chlink=y_data['chlink']))
-    if data == []:
-      return None
+            time.sleep(1)
+            print(urls)
+            y_data = youtubes(urls)
+
+            data.append(dict(url=urls, name=y_data['name'], time=times, image=img, icon=icons, title=y_data['title'],
+                             count=y_data['viewcount'], chlink=y_data['chlink']))
+    if not data:
+        return None
     else:
-      return flexdata(data)
+        return flexdata(data)
+
 
 def youtubes(url):
-  videoinfo = Video.getInfo(url)
-  title = videoinfo["title"]
-  viewcount = videoinfo["viewCount"]["text"]+"views"
-  name = videoinfo["channel"]["name"]
-  chlink = videoinfo["channel"]["link"]
-  y_data = dict(title=title,viewcount=viewcount,name=name,chlink=chlink)
+    videoinfo = Video.getInfo(url)
+    title = videoinfo["title"]
+    viewcount = videoinfo["viewCount"]["text"] + "views"
+    name = videoinfo["channel"]["name"]
+    chlink = videoinfo["channel"]["link"]
+    y_data = dict(title=title, viewcount=viewcount, name=name, chlink=chlink)
 
-  return y_data
+    return y_data
+
 
 def flexdata(datas):
     base = {
-            "type": "carousel",
-            "contents": []
-            }
-    
+        "type": "carousel",
+        "contents": []
+    }
+
     for data in datas:
         template = {
-                      "type": "bubble",
-                      "hero": {
-                        "type": "image",
-                        "url": data["image"],
-                        "size": "full",
-                        "aspectRatio": "20:13",
-                        "aspectMode": "cover",
-                        "action": {
-                          "type": "uri",
-                          "uri": data["url"]
-                        }
-                      },
-                      "body": {
-                        "type": "box",
-                        "layout": "vertical",
+            "type": "bubble",
+            "hero": {
+                "type": "image",
+                "url": data["image"],
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover",
+                "action": {
+                    "type": "uri",
+                    "uri": data["url"]
+                }
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
                         "contents": [
-                          {
-                            "type": "text",
-                            "contents": [
-                              {
-                               "type": "span",
-                              "text": data["title"]
-                              }
-                            ],
-                            "weight": "bold",
-                            "size": "xl",
-                            "wrap": False
-                          },
-                          {
-                            "type": "box",
-                            "layout": "baseline",
-                            "margin": "md",
-                            "contents": [
-                              {
+                            {
+                                "type": "span",
+                                "text": data["title"]
+                            }
+                        ],
+                        "weight": "bold",
+                        "size": "xl",
+                        "wrap": False
+                    },
+                    {
+                        "type": "box",
+                        "layout": "baseline",
+                        "margin": "md",
+                        "contents": [
+                            {
                                 "type": "icon",
                                 "size": "md",
                                 "url": 'https://schedule.hololive.tv/dist/images/icons/youtube.png',
                                 "margin": "none"
-                              },
-                              {
+                            },
+                            {
                                 "type": "text",
                                 "text": data["time"],
                                 "size": "md",
                                 "margin": "md"
-                              },
-                              {
+                            },
+                            {
                                 "type": "text",
                                 "text": data["count"],
                                 "align": "center"
-                              }
-                            ]
-                          }
+                            }
                         ]
-                      },
-                      "footer": {
+                    }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                    {
                         "type": "box",
-                        "layout": "horizontal",
+                        "layout": "vertical",
                         "contents": [
-                          {
-                            "type": "box",
-                            "layout": "vertical",
-                            "contents": [
-                              {
+                            {
                                 "type": "image",
                                 "url": data["icon"],
                                 "aspectMode": "cover",
                                 "size": "full"
-                              }
-                            ],
-                            "cornerRadius": "100px",
-                            "width": "72px",
-                            "height": "72px",
-                            "action": {
-                              "type": "uri",
-                              "uri": data["chlink"]
                             }
-                          },
-                          {
-                            "type": "box",
-                            "layout": "vertical",
-                            "contents": [
-                              {
+                        ],
+                        "cornerRadius": "100px",
+                        "width": "72px",
+                        "height": "72px",
+                        "action": {
+                            "type": "uri",
+                            "uri": data["chlink"]
+                        }
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                            {
                                 "type": "text",
                                 "contents": [
-                                  {
-                                    "type": "span",
-                                    "text": data["name"]
-                                  },
+                                    {
+                                        "type": "span",
+                                        "text": data["name"]
+                                    },
                                 ],
                                 "margin": "xxl",
                                 "size": "xl",
                                 "align": "center",
                                 "wrap": True
-                              }
-                            ]
-                          }
-                        ],
-                        "spacing": "xl",
-                        "paddingAll": "20px"
-                      }
+                            }
+                        ]
                     }
+                ],
+                "spacing": "xl",
+                "paddingAll": "20px"
+            }
+        }
         base["contents"].append(template)
     return base
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, abort
- 
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -10,30 +10,30 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
 )
 import os
+import hololive
+import compass
 
-import hololive,compass
- 
 app = Flask(__name__)
- 
-#環境変数取得
+
+# 環境変数取得
 # LINE Developersで設定されているアクセストークンとChannel Secretをを取得し、設定します。
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
- 
+
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
- 
- 
-#Webhookからのリクエストをチェックします。
+
+
+# Webhookからのリクエストをチェックします。
 @app.route("/callback", methods=['POST'])
 def callback():
     # リクエストヘッダーから署名検証のための値を取得します。
     signature = request.headers['X-Line-Signature']
- 
+
     # リクエストボディを取得します。
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
- 
+
     # handle webhook body
     # 署名を検証し、問題なければhandleに定義されている関数を呼び出す。
     try:
@@ -43,12 +43,13 @@ def callback():
         abort(400)
     # handleの処理を終えればOK
     return 'OK'
- 
-#LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
-#def以下の関数を実行します。
-#reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。 
-#第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
- 
+
+
+# LINEでMessageEvent（普通のメッセージを送信された場合）が起こった場合に、
+# def以下の関数を実行します。
+# reply_messageの第一引数のevent.reply_tokenは、イベントの応答に用いるトークンです。
+# 第二引数には、linebot.modelsに定義されている返信用のTextSendMessageオブジェクトを渡しています。
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == "test":
@@ -57,7 +58,7 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text="ok"))
     if event.message.text == "hololive":
-        if hololive.scrape() == None:
+        if hololive.scrape() is None:
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text="今は誰も配信していません"))
@@ -68,9 +69,16 @@ def handle_message(event):
     if event.message.text == "compass":
         line_bot_api.reply_message(
             event.reply_token,
-                FlexSendMessage(alt_text='compass', contents=compass.gacha()))
+            FlexSendMessage(alt_text='compass', contents=compass.gacha()))
+    if event.message.text == "mid":
+        print(event)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="ok"))
+
+
 # ポート番号の設定
 if __name__ == "__main__":
-#    app.run()
+    #    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
