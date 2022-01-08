afc28ed55b6d1a1ac218e874d5af40cf650bba12
k4my-git <kambara0229@icloud.com>
2022-01-08 23:58:01 +0900

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
+    MessageEvent, TextMessage, TextSendMessage, FlexSendMessage, RichMenu, RichMenuArea, RichMenuBounds, RichMenuSize, URIAction
 )
 import os
 import hololive
@@ -96,6 +96,20 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=bot_info.display_name))
+    if msg == "richmenu":
+        rich_menu_to_create = RichMenu(
+            size=RichMenuSize(width=2500, height=843),
+            selected=False,
+            name="Nice richmenu",
+            chat_bar_text="Tap here",
+            areas=[RichMenuArea(
+                bounds=RichMenuBounds(x=0, y=0, width=2500, height=843),
+                action=URIAction(label='Go to line.me', uri='https://line.me'))]
+            )
+        rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+        print(rich_menu_id)
+        uid = event.source.user_id
+        line_bot_api.link_rich_menu_to_user(uid, rich_menu_id)
 
 
 # ポート番号の設定
